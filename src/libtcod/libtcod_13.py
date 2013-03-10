136db69b8237efdb2ceb8985c099a802d80746e7
Dan Stuart <drestuart@gmail.com>
2013-03-10 16:15:35 -0400

    Fixed some libtcod tuts to import correctly.

--- src/libtcod/libtcod_13.py
+++ src/libtcod/libtcod_13.py
@@ -2,8 +2,9 @@
 #
 # libtcod python tutorial
 #
- 
-import libtcodpy as libtcod
+from Import import *
+libtcod = importLibtcod()
+
 import math
 import textwrap
 import shelve
@@ -1131,7 +1132,7 @@ def main_menu():
         elif choice == 2:  #quit
             break
  
-libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+libtcod.console_set_custom_font('..\\..\\fonts\\arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'python/libtcod tutorial', False)
 libtcod.sys_set_fps(LIMIT_FPS)
 con = libtcod.console_new(MAP_WIDTH, MAP_HEIGHT)
