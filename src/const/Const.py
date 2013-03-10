5a97e260617128f40c8bb38f8711473c37f6b998
Dan Stuart <drestuart@gmail.com>
2013-03-10 17:03:06 -0400

    Moved some constants to Const.py

---
+++ src/const/Const.py
@@ -0,0 +1,27 @@
+'''
+Created on Mar 10, 2013
+
+@author: dstu
+'''
+
+# Global constants
+
+# Max monsters per room
+MAX_ROOM_MONSTERS = 3
+
+# Max items per room
+MAX_ROOM_ITEMS = 2
+
+# Field of view constants
+FOV_ALGO = 0  #default FOV algorithm
+FOV_LIGHT_WALLS = True
+
+
+# Map dimensions
+MAP_WIDTH = 80
+MAP_HEIGHT = 50
+
+# Some dungeon generation constants
+ROOM_MAX_SIZE = 10
+ROOM_MIN_SIZE = 6
+MAX_ROOMS = 30
