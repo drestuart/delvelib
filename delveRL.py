c52f8e524db2e8d289aeba12da379c07cfb4a0f8
Dan Stuart <drestuart@gmail.com>
2013-03-10 16:51:04 -0400

    Created stub Game and main class files

---
+++ delveRL.py
@@ -0,0 +1,13 @@
+'''
+Created on Mar 10, 2013
+
+@author: dstu
+'''
+
+def main():
+    print '-=delveRL=-'
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/Game.py
@@ -0,0 +1,11 @@
+'''
+Created on Mar 10, 2013
+
+@author: dstu
+'''
+
+class Game(object):
+
+
+    def __init__(self, **kwargs):
+        pass
