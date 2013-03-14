04498f1292ebc9d1b73d5b98543f2318bad10623
Dan Stuart <drestuart@gmail.com>
2013-03-14 15:20:15 -0400

    Added sample Orc creature

--- src/creatures/CreatureClass.py
+++ src/creatures/CreatureClass.py
@@ -11,6 +11,7 @@ from sqlalchemy.types import String, Integer, Boolean
 import colors
 import database as db
 from ctypes.wintypes import INT
+import AIClass as AI
 
 libtcod = importLibtcod()
 
@@ -270,69 +271,22 @@ class Creature(Base):
         
         self.AI = self.AIClass()
         self.AI.setOwner(self)
+        
+    def takeTurn(self):
+        self.AI.takeTurn()
 
 
+class Orc(Creature):
+    
+    def __init__(self, **kwargs):
+        super(Orc, self).__init__(symbol = 'o', baseColor = colors.red, baseBackground = colors.black, 
+                                  name = 'orc', description = 'a hideous orc', species = 'orc',
+                                  maxHP = 10, AIClass = AI.AggressiveAI, **kwargs)
+    
+    __mapper_args__ = {'polymorphic_identity': 'orc'}
 
 
     
-#    def move(self, dx, dy):
-#        
-#        newCoords = self.coordinates + Coordinates(x = dx, y = dy)
-#        newTile = self.map.getTile(newCoords)
-#        
-#        if newTile.addCreature(self):
-#            
-#            #Remove self from the old tile
-#            oldTile = self.map.getTile(self.coordinates)
-#            oldTile.removeCreature()
-#        
-#            self.setPosition(self.map, newCoords)
-#            #self.energy -= self.moveCost
-#                        
-#            print self.name + " moves to", self.coordinates
-#            return True
-#        
-#        else:
-#            return False
-#        
-#    def setPosition(self, map, coords):
-#        self.__dict__['coordinates'] = coords
-#        self.__dict__['map'] = map
-#    
-#    def changeHP(self, amount):
-#        self.__dict__['hp'] = min(self.hp + amount, self.max_hp)
-#        if self.hp <= 0:
-#            self.deathFunction(self)
-#
-#    def heal(self, amount):
-#        #heal by the given amount
-#        self.changeHP(amount)
-#
-#    def takeDamage(self, damage):
-#        #apply damage if possible
-#        if damage > 0:
-#            self.changeHP(-damage)
-#            
-#    def passTime(self, turns = 1):
-#        print "It is " + self.name + "'s turn"
-#        for i in range(turns):
-#            self.takeTurn() 
-#
-#    def takeTurn(self):
-#        self.ai.takeTurn()
-#    
-#    def isVisible(self):
-#        return self.visible
-#    
-#    def color(self):
-#        # There will probably be some more logic here
-#        return self.baseColor
-#    
-#    def symbol(self):
-#        return self.symbol
-#    
-#    def background(self):
-#        return self.baseBackground
 
 
 
