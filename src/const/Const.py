cac5f4ac80c449237de5256cfd3b1771320b6b56
Dan Stuart <drestuart@gmail.com>
2013-03-15 19:48:22 -0400

    Reorganization in Const.py

--- src/const/Const.py
+++ src/const/Const.py
@@ -10,23 +10,38 @@ import libtcodpy as libtcod
 
 TITLE = "delveRL"
 
-# Max monsters per room
-MAX_ROOM_MONSTERS = 3
 
-# Max items per room
-MAX_ROOM_ITEMS = 2
+########################
+#
+# UI
+#
+########################
+
+SCREEN_WIDTH = 100
+SCREEN_HEIGHT = 70
+
+LIMIT_FPS = 10
+
+# Map dimensions
+MAP_WIDTH = 80
+MAP_HEIGHT = 50
+
+PANEL_HEIGHT = 20
+PANEL_WIDTH = MAP_WIDTH
+
+#MAP_WIDTH = 10
+#MAP_HEIGHT = 10
 
 # Field of view constants
 FOV_ALGO = libtcod.FOV_BASIC  #default FOV algorithm
 FOV_LIGHT_WALLS = True
 
 
-# Map dimensions
-MAP_WIDTH = 80
-MAP_HEIGHT = 50
-
-#MAP_WIDTH = 10
-#MAP_HEIGHT = 10
+########################
+#
+# Dungeon Generation
+#
+########################
 
 # Some dungeon generation constants
 ROOM_MAX_SIZE = 10
@@ -36,13 +51,14 @@ DUNGEON_MARGIN = 2
 MAX_ROOMS_AND_CORRIDORS = 100
 ROOM_CHANCE = 75
 
+# Max monsters per room
+MAX_ROOM_MONSTERS = 3
+
+# Max items per room
+MAX_ROOM_ITEMS = 2
+
 #ROOM_MAX_SIZE = 3
 #ROOM_MIN_SIZE = 1
 #MAX_ROOMS = 3
 #DUNGEON_MARGIN = 0
 
-
-SCREEN_WIDTH = 100
-SCREEN_HEIGHT = 70
-
-LIMIT_FPS = 10
