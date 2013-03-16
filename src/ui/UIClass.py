d1ed616ce370d6f9a6d5e34f4d7343e346398abd
Dan Stuart <drestuart@gmail.com>
2013-03-15 20:09:09 -0400

    Partial implement of the message window

--- src/ui/UIClass.py
+++ src/ui/UIClass.py
@@ -5,9 +5,10 @@ Created on Mar 12, 2013
 '''
 
 from Import import *
-import os.path
 import Const as C
 import colors
+import os.path
+import textwrap
 
 libtcod = importLibtcod()
 
@@ -31,7 +32,42 @@ class UI(object):
     def __init__(self, **kwargs):
         self.currentLevel = kwargs.get('level', None)
         self.player = kwargs.get('player', None)
+        self.panel = libtcod.console_new(C.PANEL_WIDTH, C.PANEL_HEIGHT)
+
+        self.msgs = []
         
+    
+    def render_bar(self, x, y, totalWidth, name, value, maximum, barColor, backColor):
+        #render a bar (HP, experience, etc). first calculate the width of the bar
+        barWidth = int(float(value) / maximum * totalWidth)
+     
+        #render the background first
+        libtcod.console_set_default_background(self.panel, backColor)
+        libtcod.console_rect(self.panel, x, y, totalWidth, 1, False, libtcod.BKGND_SCREEN)
+     
+        #now render the bar on top
+        libtcod.console_set_default_background(self.panel, barColor)
+        if barWidth > 0:
+            libtcod.console_rect(self.panel, x, y, barWidth, 1, False, libtcod.BKGND_SCREEN)
+            
+        #finally, some centered text with the values
+        libtcod.console_set_default_foreground(self.panel, libtcod.white)
+        libtcod.console_print_ex(self.panel, x + totalWidth / 2, y, libtcod.BKGND_NONE, libtcod.CENTER,
+            name + ': ' + str(value) + '/' + str(maximum))
+        
+    def message(self, newMsg, color = colors.white):
+        #split the message if necessary, among multiple lines
+        newMsgLines = textwrap.wrap(newMsg, C.MSG_WIDTH)
+     
+        for line in newMsgLines:
+            #if the buffer is full, remove the first line to make room for the new one
+            if len(self.msgs) == C.MSG_HEIGHT:
+                del self.msgs[0]
+     
+            #add the new line as a tuple, with the text and the color
+            self.msgs.append( (line, color) )
+    
+    
     def createWindow(self):
         libtcod.console_set_custom_font(DEFAULT_FONT, libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
         libtcod.console_init_root(C.SCREEN_WIDTH, C.SCREEN_HEIGHT, C.TITLE, False)
@@ -44,6 +80,7 @@ class UI(object):
         libtcod.console_set_default_background(self.mapConsole, libtcod.BKGND_NONE)
         
         libtcod.console_set_keyboard_repeat(500, 250)
+        
         
     def getCurrentLevel(self):
         return self.currentLevel
@@ -59,12 +96,29 @@ class UI(object):
             libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS|libtcod.EVENT_MOUSE,key,mouse)
             libtcod.console_clear(self.mapConsole)
             
+            #prepare to render the GUI panel
+            libtcod.console_set_default_background(self.panel, libtcod.black)
+            libtcod.console_clear(self.panel)
+            
+            y = 1
+            for (line, color) in self.msgs:
+                libtcod.console_set_default_foreground(self.panel, color)
+                libtcod.console_print_ex(self.panel, C.MSG_X, y, libtcod.BKGND_NONE, libtcod.LEFT, line)
+                y += 1
+         
+            #show the player's stats
+#            render_bar(1, 1, BAR_WIDTH, 'HP', player.fighter.hp, player.fighter.max_hp,
+#                libtcod.light_red, libtcod.darker_red)
+         
+            #blit the contents of "panel" to the root console
+            libtcod.console_blit(self.panel, 0, 0, C.SCREEN_WIDTH, C.PANEL_HEIGHT, 0, C.PANEL_X, C.PANEL_Y)
+            
             self.currentLevel.computeFOV(self.player.getX(), self.player.getY(), 0)
 
 #            print "Drawing"
             self.currentLevel.draw()
     #        libtcod.console_blit(mapConsole, 0, 0, C.MAP_WIDTH, C.MAP_HEIGHT, 0, 0, 0)
-            libtcod.console_blit(self.mapConsole, 0, 0, C.MAP_WIDTH, C.MAP_HEIGHT, 0, 0, 0)
+            libtcod.console_blit(self.mapConsole, 0, 0, C.MAP_WIDTH, C.MAP_HEIGHT, 0, C.MAP_X, C.MAP_Y)
     
             libtcod.console_flush()
             
