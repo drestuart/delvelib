86c19cd1fc0fc5cacdab03363460770188d2ea76
Dan Stuart <drestuart@gmail.com>
2013-03-19 12:35:21 -0400

    Basic item and inventory implementation

--- src/items/InventoryClass.py
+++ src/items/InventoryClass.py
@@ -7,38 +7,47 @@ Created on May 17, 2012
 from sqlalchemy.ext.declarative import declared_attr
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.schema import Column, ForeignKey
-from sqlalchemy.types import Integer
+from sqlalchemy.types import Integer, String
+import ItemClass as I
+import colors
 import database as db
 
 Base = db.saveDB.getDeclarativeBase()
 
 class Inventory(Base):
     '''
-    The inventory class.  Represents a collection of items, either in a container or held by a person.
+    The inventory class.  Represents a collection of items, either on a tile, in a container or held by a person.
     Also provides some convenience methods.
     '''
 
     __tablename__ = "inventories"
     __table_args__ = {'extend_existing': True}
     
-    id = Column(Integer, primary_key=True)
-    items = relationship("Item", backref=backref("container", uselist=False), primaryjoin="Inventory.id==Item.containerId")
-#    personId = Column(Integer, ForeignKey("people.id"))
-#    person = relationship("Person", backref=backref("inventory", uselist=False))
-    
-    def __init__(self, items = [], person = None):
+    def __init__(self, **kwargs):
         
-        for item in items:
-            self.addItem(item)
-            
-        self.person = person
+        self.items = []
+    
+    id = Column(Integer, primary_key=True)
+    items = relationship("Item", backref=backref("container", uselist=False), primaryjoin="Inventory.id == Item.containerId")
+    inventoryType = Column(String)
+    
+    # If this inventory belongs to an item
+    containingItemId = Column(Integer, ForeignKey("items.id", use_alter=True, name='containing_item_fk'))
+    containingItem = relationship("Item", uselist=False, backref=backref("myInventory", uselist=False), primaryjoin="Inventory.id == Item.myInventoryId")
 
+    
+    
+    __mapper_args__ = {
+        'polymorphic_on':inventoryType,
+        'polymorphic_identity':'item'
+    }
+    
     
     def printContents(self):
         print self.items
     
     def addItem(self, itemIn):
-        if not self.items:
+        if self.items is None:
             self.items = []
         if not itemIn in self.items:
             self.items.append(itemIn)
@@ -55,67 +64,46 @@ class Inventory(Base):
         
         raise ValueError(itemIn + " does not exist in container " + self)
     
-    def getId(self):
-        return self.id
-    
-    def setId(self, val):
-        self.id = val
-        return self
-    
-    def getItems(self):
-        return self.items
-    
-    def setItems(self, val):
-        self.items = val
-        return self
-    
-    def getPersonId(self):
-        return self.personId
-    
-    def setPersonId(self, val):
-        self.personId = val
-        return self
-    
-    def getPerson(self):
-        return self.person
-    
-    def setPerson(self, val):
-        self.person = val
-        if val:
-            self.setContainingItem(None)
-            self.setPersonId(val.getId())
-        else:
-            self.setPersonId(None)
-        return self
-    
-    def getContainingItemId(self):
-        return self.containingItemId
-    
-    def setContainingItemId(self, val):
-        self.containingItemId = val
-        return self
-    
-    def getContainingItem(self):
-        return self.containingItem
-    
-    def setContainingItem(self, val):
-        self.containingItem = val
-        if val:
-            self.setPerson(None)
-            self.setContainingItemId(val.getId())
-        else:
-            self.setContainingItemId(None)
-        return self
-    
     def pop(self):
         if len(self.items) == 0:
             return None
         itemToPop = self.items[0]
         return self.removeItem(itemToPop)
-    
-def main():
-    pass
 
-if __name__ == "__main__":
+    def getItems(self):
+        return self.items
+
+
+    def getContainingItem(self):
+        return self.containingItem
+
+
+    def setItems(self, value):
+        self.items = value
+
+
+    def setContainingItem(self, value):
+        self.containingItem = value
+
+    
+
+
+def main():
+    db.saveDB.start(True)
+    
+    item1 = I.Item(symbol = '!', color = colors.white)
+    item2 = I.Item(symbol = '!', color = colors.red)
+    
+    inv1 = Inventory()
+    inv1.addItem(item1)
+    inv1.addItem(item2)
+    
+    db.saveDB.save(inv1)
+
+
+
+
+    
+if __name__ == '__main__':
     main()
     
--- src/items/ItemClass.py
+++ src/items/ItemClass.py
@@ -4,10 +4,13 @@ Created on Mar 10, 2013
 @author: dstu
 '''
 
-from sqlalchemy.orm import relationship
+from Import import *
 from sqlalchemy.schema import Column, ForeignKey
 from sqlalchemy.types import String, Integer, Float
+import colors
 import database as db
+
+libtcod = importLibtcod()
 
 Base = db.saveDB.getDeclarativeBase()
 
@@ -20,37 +23,49 @@ class Item(Base):
 
 
     def __init__(self, **kwargs):
-        self.weight = kwargs['weight']
-        self.possessor = kwargs['possessor']
-        self.type_ = kwargs['type_']
-        self.onUse = kwargs['onUse']
-        self.onDrop = kwargs['onDrop']
-        self.onPickup = kwargs['onPickup']
+        self.weight = kwargs.get('weight', 0)
+        self.material = kwargs.get('material', None)
+        
+        self.symbol = kwargs['symbol']
+        self.color = kwargs['color']
+        self.backgroundColor = kwargs.get('baseBackground', colors.black)
+        
+        self.colorR = self.color.r
+        self.colorG = self.color.g
+        self.colorB = self.color.b
+        
+        self.backgroundColorR = self.backgroundColor.r
+        self.backgroundColorG = self.backgroundColor.g
+        self.backgroundColorB = self.backgroundColor.b
+        
 
     id = Column(Integer, primary_key=True)
     weight = Column(Float)
-    type_ = Column(String)
-
-    possessor = relationship("Creature", primaryjoin="Creature.id==Item.possessorId")
-    possessorId = Column(Integer, ForeignKey("creatures.id"))
-#    containerId = Column(Integer, ForeignKey("inventories.id"))
+    itemType = Column(String)
+    material = Column(String)
     
-    # For items that have an inventory
-#    inventoryId = Column(Integer, ForeignKey("inventories.id"))
-#    inventory = relationship("Inventory", uselist=False, primaryjoin="Inventory.id==Item.inventoryId")
+    symbol = Column(String(length=1, convert_unicode = False))
+    
+    colorR = Column(Integer)
+    colorG = Column(Integer)
+    colorB = Column(Integer)
+    
+    backgroundColorR = Column(Integer)
+    backgroundColorG = Column(Integer)
+    backgroundColorB = Column(Integer)
+
+    containerId = Column(Integer, ForeignKey("inventories.id"))
+    
+    # For items that have an inventory of their own
+    myInventoryId = Column(Integer, ForeignKey("inventories.id", use_alter=True, name='my_inventory_fk'))
+#    inventory = relationship("Inventory", uselist=False, backref=backref("containingItem", uselist=False), primaryjoin="Inventory.id==Item.inventoryId")
 
     
     __mapper_args__ = {
-        'polymorphic_on':type_,
+        'polymorphic_on':itemType,
         'polymorphic_identity':'item'
     }
     
-#    def getContainer(self):
-#        return self.container
-#    
-#    def setContainer(self, val):
-#        self.container = val
-#        return self
     
     def drop(self):
         # TODO: Handle universal dropping logic
@@ -62,31 +77,119 @@ class Item(Base):
         
     def use(self):
         self.onUse()
-    
-    
-    def getId(self):
-        return self.id
 
-    def setId(self, val):
-        self.id = val
-        return self
-    
     def getWeight(self):
         return self.weight
+
+
+    def getMaterial(self):
+        return self.material
+
+
+    def getSymbol(self):
+        return self.symbol
+
+
+    def setWeight(self, value):
+        self.weight = value
+
+
+    def setMaterial(self, value):
+        self.material = value
+
+
+    def setSymbol(self, value):
+        self.symbol = value
+
+    def getColor(self):        
+        if self.__dict__.get('color', None):
+            return self.color
+        else:
+            self.color = libtcod.Color(self.colorR, self.colorG, self.colorB)
+            return self.color
+
+    def getBackgroundColor(self):        
+        if self.__dict__.get('backgroundColor', None):
+            return self.backgroundColor
+        else:
+            self.backgroundColor = libtcod.Color(self.backgroundColorR, self.backgroundColorG, self.backgroundColorB)
+            return self.backgroundColor
+        
+    def setColor(self, value):
+        self.color = value
+        self.setColorB(value.b)
+        self.setColorG(value.g)
+        self.setColorR(value.r)
+
+
+    def setBackgroundColor(self, value):
+        self.backgroundColor = value
+        self.setBackgroundColorB(value.b)
+        self.setBackgroundColorG(value.g)
+        self.setBackgroundColorR(value.r)
+
+    def getColorR(self):
+        return self.colorR
+
+
+    def getColorG(self):
+        return self.colorG
+
+
+    def getColorB(self):
+        return self.colorB
+
+
+    def getBackgroundColorR(self):
+        return self.backgroundColorR
+
+
+    def getBackgroundColorG(self):
+        return self.backgroundColorG
+
+
+    def getBackgroundColorB(self):
+        return self.backgroundColorB
+
+
+    def setColorR(self, value):
+        self.colorR = value
+
+
+    def setColorG(self, value):
+        self.colorG = value
+
+
+    def setColorB(self, value):
+        self.colorB = value
+
+
+    def setBackgroundColorR(self, value):
+        self.backgroundColorR = value
+
+
+    def setBackgroundColorG(self, value):
+        self.backgroundColorG = value
+
+
+    def setBackgroundColorB(self, value):
+        self.backgroundColorB = value
+
     
-    def setWeight(self, val):
-        self.weight = val
-        return self
     
-    def getPossessor(self):
-        return self.possessor
-    
-    def setPossessor(self, val):
-        self.possessor = val
-        return self
     
 def main():
-    pass
+    import InventoryClass as I
+    
+    db.saveDB.start(True)
+    
+    
+    item1 = Item(symbol = '!', color = colors.white)
+    item2 = Item(symbol = '!', color = colors.red)
+    
+    db.saveDB.save(item1)
+    db.saveDB.save(item2)
+
 
 
     
