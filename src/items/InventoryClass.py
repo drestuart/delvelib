d54e6e2be42657309717cc8613aacd9d83c03c51
Dan Stuart <drestuart@gmail.com>
2013-03-19 11:40:02 -0400

    Imported InventoryClass.py from guilds project

--- src/items/InventoryClass.py
+++ src/items/InventoryClass.py
@@ -1,44 +1,121 @@
-###############################################################
-#
-# OLD STUFF
-#
-###############################################################
+'''
+Created on May 17, 2012
 
-# External imports
-import libtcodpy as libtcod
-from keys import *
-from menu import *
-from GetSetClass import *
+@author: dstu
+'''
 
-INVENTORY_WIDTH = 50
+from sqlalchemy.ext.declarative import declared_attr
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.schema import Column, ForeignKey
+from sqlalchemy.types import Integer
+import database as db
 
-class ItemInventory(GetSet):
-    # A list of items, with some organization logic and the ability to choose an item out of a menu.
-    def __init__(self):
-        self.__dict__['items'] = []  # This will probably be a dict at some point
-        
-    def __len__(self):
-        return len(self.items)
+Base = db.saveDB.getDeclarativeBase()
+
+class Inventory(Base):
+    '''
+    The inventory class.  Represents a collection of items, either in a container or held by a person.
+    Also provides some convenience methods.
+    '''
+
+    __tablename__ = "inventories"
+    __table_args__ = {'extend_existing': True}
     
-    def add(self, item):
-        self.items.append(item)
+    id = Column(Integer, primary_key=True)
+    items = relationship("Item", backref=backref("container", uselist=False), primaryjoin="Inventory.id==Item.containerId")
+#    personId = Column(Integer, ForeignKey("people.id"))
+#    person = relationship("Person", backref=backref("inventory", uselist=False))
+    
+    def __init__(self, items = [], person = None):
         
-    def __getitem__(self, ind):
-        return self.items[ind]
-        
-        
-# The inventory menu
-def inventoryMenu(header):
-    #show a menu with each item of the inventory as an option
-    if len(inventory) == 0:
-        options = ['Inventory is empty.']
-    else:
-        options = [item.name for item in inventory]
- 
-    index = menu(header, options, INVENTORY_WIDTH)
+        for item in items:
+            self.addItem(item)
+            
+        self.person = person
 
-    #if an item was chosen, return it
-    if index is None or len(inventory) == 0:
-        return None
-    return inventory[index].item
+    
+    def printContents(self):
+        print self.items
+    
+    def addItem(self, itemIn):
+        if not self.items:
+            self.items = []
+        if not itemIn in self.items:
+            self.items.append(itemIn)
+#            itemIn.setContainer(self)
+            return self
+        
+        raise ValueError(itemIn + " already exists in container " + self)
+    
+    def removeItem(self, itemIn):
+        if itemIn in self.items:
+            self.items.remove(itemIn)
+#            itemIn.setContainer(None)
+            return itemIn
+        
+        raise ValueError(itemIn + " does not exist in container " + self)
+    
+    def getId(self):
+        return self.id
+    
+    def setId(self, val):
+        self.id = val
+        return self
+    
+    def getItems(self):
+        return self.items
+    
+    def setItems(self, val):
+        self.items = val
+        return self
+    
+    def getPersonId(self):
+        return self.personId
+    
+    def setPersonId(self, val):
+        self.personId = val
+        return self
+    
+    def getPerson(self):
+        return self.person
+    
+    def setPerson(self, val):
+        self.person = val
+        if val:
+            self.setContainingItem(None)
+            self.setPersonId(val.getId())
+        else:
+            self.setPersonId(None)
+        return self
+    
+    def getContainingItemId(self):
+        return self.containingItemId
+    
+    def setContainingItemId(self, val):
+        self.containingItemId = val
+        return self
+    
+    def getContainingItem(self):
+        return self.containingItem
+    
+    def setContainingItem(self, val):
+        self.containingItem = val
+        if val:
+            self.setPerson(None)
+            self.setContainingItemId(val.getId())
+        else:
+            self.setContainingItemId(None)
+        return self
+    
+    def pop(self):
+        if len(self.items) == 0:
+            return None
+        itemToPop = self.items[0]
+        return self.removeItem(itemToPop)
+    
+def main():
+    pass
 
+if __name__ == "__main__":
+    main()
+    
