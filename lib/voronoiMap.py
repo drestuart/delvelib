55c0082535e6ddb8351e387d9d2ce87754d8e1d6
Dan Stuart <dstuart@barracuda.com>
2014-02-24 18:05:16 -0500

    Improvements to voronoiMap lib: made more object-oriented, added function to compute adjacency matrix

--- lib/voronoiMap.py
+++ lib/voronoiMap.py
@@ -11,48 +11,122 @@
 
 import random
 import math
+import bline
 
-def generate_voronoi_map(width, height, num_cells, use_symbols = False):
-	imgx, imgy = width, height
-	nx = []
-	ny = []
-	symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')',
-			   '.', ',', '<', '>', '?', ':', '|', '{', ']', '+']
-	points = []
-	vmap = ""
-	for i in range(num_cells):
-		randx = random.randrange(imgx)
-		randy = random.randrange(imgy)
+class VMapRegion(object):
+	
+	def __init__(self, x, y, symb):
+		self.memberPoints = []
+		self.symbol = symb
+		self.centerPoint = x, y
+		self.x = x
+		self.y = y
 		
-		nx.append(randx)
-		ny.append(randy)
-		points.append((randx,randy))
+	def distance(self, tox, toy):
+		return math.hypot(self.x - tox, self.y - toy)
+	
+	def addPoint(self, x, y):
+		self.memberPoints.append((x, y))
 		
-	for y in range(imgy):
-		for x in range(imgx):
-			dmin = math.hypot(imgx-1, imgy-1)
-			j = -1
-			for i in range(num_cells):
-				d = math.hypot(nx[i]-x, ny[i]-y)
-				if d < dmin:
-					dmin = d
-					j = i
+	def containsPoint(self, x, y):
+		return (x, y) in self.memberPoints
+
+class VMap(object):
+	
+	def __init__(self, width, height, num_cells):
+		self.width = width
+		self.height = height
+		self.num_cells = num_cells
+		self.vmap = ""
+		self.centerPoints = []
+		self.adjacency = {}
+		self.regions = []
+		self.pointsToRegions = {}
+	
+	def generate_voronoi_map(self, use_symbols = False):
+
+		symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')',
+				   '.', ',', '<', '>', '?', ':', '|', '{', ']', '+']
+		centerPoints = []
+		vmap = ""
+		
+		for i in range(self.num_cells):
+			randx = random.randrange(self.width)
+			randy = random.randrange(self.height)
+			
 			if use_symbols:
-				vmap += symbols[j] + " "
+				symb = symbols[i]
 			else:
-				vmap += str(j) + " "
+				symb = str(i)
+
+			centerPoints.append((randx, randy))
+			newRegion = VMapRegion(randx, randy, symb)
+			self.regions.append(newRegion)
+
+		for y in range(self.height):
+			for x in range(self.width):
+				dmin = math.hypot(self.width-1, self.height-1)
+				closestRegion = None
+				for reg in self.regions:
+					d = reg.distance(x, y)
+					if d < dmin:
+						closestRegion = reg
+						dmin = d
 				
-		vmap += "\n"
-	return points, vmap
+				vmap += closestRegion.symbol + " "
+				closestRegion.addPoint(x, y)
+				self.pointsToRegions[(x, y)] = closestRegion
+					
+			vmap += "\n"
+			
+		self.centerPoints = centerPoints
+		self.vmap = vmap
+		return centerPoints, vmap
+	
+	def getAdjacency(self):
+		# Initialize adjacency matrix
+		for reg in self.regions:
+			self.adjacency[reg] = set()
+			
+		for y in range(self.height):
+			for x in range(self.width):
+				
+				thisReg = self.pointsToRegions[(x,y)]
+				for i in (-1, 0, 1):
+					for j in (-1, 0, 1):
+						if (i == 0 and j == 0):
+							continue
+						if (x + i < 0 or y + j < 0 or x + i > self.width-1 or y + j > self.height-1):
+							continue
+						
+						adjReg = self.pointsToRegions[(x + i, y + j)]
+						if not (adjReg is thisReg):
+							self.adjacency[thisReg].add(adjReg)
+							self.adjacency[adjReg].add(thisReg)
+
+	def getAdjacentRegions(self, reg):
+		return self.adjacency[reg]
+	
+	def printAdjacency(self):
+		for key, values in self.adjacency.iteritems(): 
+			print key.symbol, "=> [",
+			for v in values:
+				print v.symbol,
+			print "]\n"
+		
 
 def main():
 	
-	points, vmap = generate_voronoi_map(50, 50, 20, True)
+	vmapObj = VMap(50, 50, 20)
+	points, vmap = vmapObj.generate_voronoi_map(True)
+	vmapObj.getAdjacency()
 	
 	print "Done!\n"
 	print points
 	print "\n"
 	print vmap
+	print "\n"
+	vmapObj.printAdjacency()
 
 
 if __name__ == '__main__':
