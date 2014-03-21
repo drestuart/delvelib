4d73b7bfe271240e9051de29efcb75540cfe9414
Dan Stuart <dstuart@barracuda.com>
2014-03-21 16:26:40 -0400

    Moving Eriu-specific code out

--- src/world/WorldMapClass.py
+++ src/world/WorldMapClass.py
@@ -10,9 +10,8 @@ from sqlalchemy.types import String, Integer
 
 import LevelClass as L
 from MapTileClass import Forest, Field, Plain, Mountain, Town
-from PlayerClass import Player
 import Util as U
-from VoronoiMap import *
+from VoronoiMap import VMap
 import database as db
 import random
 import Const as C
@@ -176,56 +175,7 @@ class WorldMap(L.MapBase):
         self.placeCreature(player, tile)
         
     def buildMap(self):
-        ''' Oh here we go. '''
-        
-        vmap = VMap(self.width, self.height, self.num_regions)
-
-        vmap.generate_voronoi_map()
-        
-        regions = vmap.regions
-        centerpoints = vmap.centerPoints
-        adj = vmap.getAdjacency()
-        
-        # Overlay template, add tiles
-        
-        
-        # Add rivers and other features
-        
-        
-        
-        # Create regions and tile objects
-        for region in regions:
-            newRegion = Region()
-            
-            tiletype = newRegion.getTileType() 
-            
-            for (x, y) in region.memberPoints:
-                # Skip tiles we've already added
-                if self.hasTile[x][y]: 
-                    tile = self.getTile(x, y)
-                    newRegion.addTile(tile)
-                
-                else:
-                    newTile = tiletype(x, y)
-                    newRegion.addTile(newTile)
-                    self.addTile(newTile)
-            
-            # Add some towns to this region
-            numTowns = random.randint(C.MIN_TOWNS_PER_REGION, C.MAX_TOWNS_PER_REGION)
-            townTiles = random.sample(newRegion.mapTiles, numTowns)
-            
-            for tile in townTiles:
-                # More probably needs to happen here
-                townx, towny = tile.getXY()
-                newTownTile = Town(townx, towny)
-                newRegion.replaceTile(newTownTile)
-                self.replaceTile(newTownTile)
-                
-
-
-        # Finish up
-        self.buildTileArray()
-#         db.saveDB.save(self)
+        raise NotImplementedError("buildMap() not implemented, use a subclass")
 
 
 
