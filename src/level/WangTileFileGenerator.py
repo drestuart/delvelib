0b2af7f17e3fc960b2e5f3f07bee71aaac1ab5bb
Dan Stuart <dstuart@barracuda.com>
2014-07-21 14:30:07 -0400

    Added a script to generate a basic wang tile file

---
+++ src/level/WangTileFileGenerator.py
@@ -0,0 +1,30 @@
+'''
+Created on Jul 21, 2014
+
+@author: dstuart
+'''
+
+from itertools import product
+
+# File header parameters
+width = 7
+height = 7
+glyphs = '.~'
+
+print "width:", width, "height:", height
+print "glyphs:", glyphs
+print
+
+numConstraints = 4
+
+prod = product('12', repeat=numConstraints)
+
+for cons in prod:
+    constraints = "".join(cons)
+    print "*TILE*"
+    print "constraints:", constraints
+    
+    for y in range(height):
+        print glyphs[0]*width
+    
+    print "\n"
